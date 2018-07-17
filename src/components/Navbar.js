6c8e42250058601d8fef25b168726d6bee5493f4
Josh Weaver <jweaver@ministrysync.com>
2018-07-16 21:53:11 -0500

    updates style and pages

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,36 +1,32 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
-import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
+import facebook from '../img/facebook.svg'
+import logo from '../img/the-mat-logo.svg'
 
 const Navbar = () => (
   <nav className="navbar is-transparent">
     <div className="container">
       <div className="navbar-brand">
         <Link to="/" className="navbar-item">
-          <figure className="image">
-            <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
-          </figure>
+        <img src={logo} alt="The Mat" style={{ height: '88px' }} />
         </Link>
       </div>
       <div className="navbar-start">
         <Link className="navbar-item" to="/about">
           About
         </Link>
-        <Link className="navbar-item" to="/products">
-          Products
-        </Link>
       </div>
       <div className="navbar-end">
         <a
           className="navbar-item"
-          href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
+          href="https://www.facebook.com/mtmorrismat/?rc=p"
           target="_blank"
+          title="Check The Mat out on Facebook"
           rel="noopener noreferrer"
         >
           <span className="icon">
-            <img src={github} alt="Github" />
+            <img src={facebook} alt="Check The Mat out on Facebook" />
           </span>
         </a>
       </div>
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -7,7 +7,7 @@ import './all.sass'
 
 const TemplateWrapper = ({ children }) => (
   <div>
-    <Helmet title="Home | Gatsby + Netlify CMS" />
+    <Helmet title="The Mat" />
     <Navbar />
     <div>{children()}</div>
   </div>
